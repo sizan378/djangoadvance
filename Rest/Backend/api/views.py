18981e2d146932697aeed653ba7b62a3df60ad9d
sizan378 <sizanmahmud08@gmail.com>
2022-06-06 07:45:04 +0600

    add api content and JWT auth

--- Rest/Backend/api/views.py
+++ Rest/Backend/api/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.renderers import JSONRenderer
+from django.http import JsonResponse,HttpResponse
+from .models import Student
+from .serializers import StudentSerializers
 
 
 def api_home(reqeust):
     return JsonResponse({"Message": "Hi this is Django"})
+
+def student(request):
+    stu = Student.objects.all()
+    serilizer = StudentSerializers(stu, many=True)
+    json_data = JSONRenderer().render(serilizer.data)
+    # return JsonResponse(serilizer.data, safe=False)
+    return HttpResponse(json_data,content_type='application/json')
